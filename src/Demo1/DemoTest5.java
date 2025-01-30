f5648fd85909b10f3536a856429e010474f40dea
ahabi <ahabi@Abdullah.fios-router.home>
2025-01-29 20:58:38 -0500

    Implemented add Mothed ()

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -4,7 +4,14 @@ public class DemoTest5 {
 	
 	public static void main (String args[]) {
 		
-		System.out.println("Java");
+		System.out.println("Abdullah");
+		
+		
 
 }
+	
+	public void add(int a, int b) {
+		
+		System.out.println(a + b);
+	}
 }
